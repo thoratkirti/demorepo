2f1aa836da1f1d99aaa15ad8482ad40ab2fffaa9
Kirti Thorat <you@example.com>
2026-09-01 15:46:54 +0530

    Adding Project

---
+++ src/com/skillio/Project.java
@@ -0,0 +1,10 @@
+package com.skillio;
+
+public class Project {
+	 
+public static void main(String[] args) {
+	System.out.println("Hello Skillio");
+}
+	}
+
+
---
+++ src/com/skillio/Test.java
@@ -0,0 +1,12 @@
+package com.skillio;
+
+public class Test {
+public static void main(String[] args) {
+	int x=10;
+	System.out.println("Hello World");
+	for(x=1;x<10;x++ ) 
+	{
+		System.out.println("For loop");
+	}
+}
+}
